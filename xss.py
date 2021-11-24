2f8bb79c14e74b7d31367a0ae6e093512b51b260
minhdt72 <87958877+minhdt72@users.noreply.github.com>
2021-11-24 09:14:25 +0700

    Update xss.py

--- xss.py
+++ xss.py
@@ -1,52 +1,65 @@
 import argparse
 import requests
-import urllib.parse
 import re
+import urllib.parse
 
-def get_method(url,params):
+def get_method(url):
     print('[-] Method: GET')
     print(f'\t[+] url: {url}')
-    print(f'\t[+] payload: {params}')
-    url = url + params
-    response = requests.get(url, params=params)
     count = 0
-    if params in response.text:
-        print("\t[+] Detected XSS ")
-        count += 1
-    print(f'[-]Detect {count} param contain XSS in {url} ')
+    payload = open('payload.txt', 'r')
 
-def post_method(url,params):
+    for pay in payload:
+        target = url + pay
+        response = requests.get(target).content.decode().lower()
+        print(f'\t[+] payload: {pay}')
+
+        if re.search(r'script.*>.*<.*/.*script', response):
+            print("\t[*] Detected XSS ")
+            count += 1
+            break
+        else:
+            print("\t[--] Continue scanning")
+            continue
+    print(f'[-]Detect {count} param contain XSS in {url}')
+
+
+def post_method(url, params):
     print('[-] Method: POST')
     print(f'\t[+] url: {url}')
-    print(f'\t[+] data: {params}')
-    payload_list = []
-    data = urllib.parse.parse_qs(str(params))
-
-    for key, value in data.items():
-        data[key] = value[0]
-        val = value[0].lower()
-        if re.search(r'script.*>.*<.*/.*script', val) is not None:
-            val = re.findall(r'script.*>.*<.*/.*script', val)[0]
-            payload_list.append(val)
-
-    response = requests.post(url, data=data).content.decode().lower()
+    print(f'\t[+] Data: {params}')
+    payload = open('payload.txt', 'r', encoding='UTF-8')
     count = 0
 
-    for payload in payload_list:
-        if payload in response:
-            print("\t[+] Detected XSS ")
+    data = urllib.parse.parse_qs(str(params))
+
+    for pay in payload:
+        key = list(data.keys())[0]
+        print(f'\t[+] Parameter: {key}')
+        data[key] = pay
+
+        response = requests.post(url, data=data).content.decode().lower()
+
+        if re.search(r'.*<.*script.*>.*<.*/.*script.*>.*', response):
+            print(f"\t[+] payload: {pay}")
+            print("\t[*] Detected XSS ")
             count += 1
-    print(f'[-]Detect {count} param contain XSS in {url} ')
+            break
+        else:
+            print(f"\t[+] payload: {pay}")
+            print("\t[--] Continue scanning")
+            continue
+
+    print(f'[-]Detect {count} param contain XSS in {url}')
 
 if __name__ =="__main__":
     parser = argparse.ArgumentParser(description="option")
     parser.add_argument('-u', '--Url',  help='url')
-    parser.add_argument('-g', '--GET',  help='Method GET')
+    parser.add_argument('-g', '--GET',  help='Method GET', nargs='*')
     parser.add_argument('-p', '--POST', help='Method POST')
     args = parser.parse_args()
 
     if args.GET is not None:
-        get_method(args.Url, params=args.GET)
-        
+        get_method(args.Url)
     elif args.POST is not None:
         post_method(args.Url, params=args.POST)
